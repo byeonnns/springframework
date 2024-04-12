7f9e835e471d0d99c46fb73ae3281ff71843810e
byeonnns <tjdwns3823@naver.com>
2024-04-12 19:17:42 +0900

    Feat: 4월 12일까지 학습 내용 추가

--- src/main/java/com/mycompany/springframework/controller/Ch02Controller.java
+++ src/main/java/com/mycompany/springframework/controller/Ch02Controller.java
@@ -1,16 +1,29 @@
 package com.mycompany.springframework.controller;
 
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.json.JSONObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.mycompany.springframework.dto.FileInfo;
+import com.mycompany.springframework.interceptor.Auth;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -88,4 +101,99 @@ public class Ch02Controller {
 	      pw.flush();
 	      pw.close();
 	   }
+	   
+	@GetMapping("/modelAndViewReturn")
+	public ModelAndView modelAndViewReturn(String chNum) {
+		log.info("modelAndViewReturn() 실행");
+		ModelAndView mav = new ModelAndView();
+		mav.addObject("chNum", chNum);
+		mav.addObject("login", true);
+		mav.addObject("userName", "감자바");
+		mav.setViewName("ch02/modelAndViewReturn");
+			   
+		return mav;
+	}
+	   
+	@GetMapping("/voidReturn")
+	public void imageDownload(HttpServletRequest request, HttpServletResponse response) throws IOException {
+	   // 다운로드할 이미지 파일 선택
+	   String fileName = "photo2.jpg";
+	   
+	   // 실제 파일의 경로 얻기
+	   String filePath = "/resources/image/photos/photo2.jpg";
+	   String fileSystemPath = request.getServletContext().getRealPath(filePath); // 파일의 절대경로를 return 받음 (.getRealPath)
+	   log.info("fileSystemPath: " + fileSystemPath);
+	   
+	   // 파일의 MIME 타입 (=content Type) 얻기
+	   String mimeType = request.getServletContext().getMimeType(fileSystemPath); // return -> image/jpeg
+	   log.info("mimeType: " + mimeType);
+	   
+	   // 파일 데이터를 읽는 입력 스트림을 얻기
+	   InputStream is = new FileInputStream(fileSystemPath);
+	   
+	   // 응답 헤더에 Content-Type 값을 지정
+	   response.setContentType(mimeType); // content-type : image/jpeg
+	   
+	   // 응답 본문에 데이터를 넣는 출력 스트림을 얻기
+	   OutputStream os = response.getOutputStream();
+	   
+	   // 응답 본문에 데이터 싣기
+	   // 입력 스트림에서 데이터를 읽고 -> 출력 스트림으로 데이터를 쓰기
+	   // 방법 1
+	   /*byte[] bytes = new byte[1024];
+	   while(true) {
+	      int readNum = is.read(bytes);
+	      if (readNum == -1) break;
+	      os.write(bytes, 0, readNum);
+	   }
+	   os.flush();
+	   is.close();
+	   os.close();*/
+	   
+	   FileCopyUtils.copy(is, os);
+	   
+	}
+	   
+	   
+	@GetMapping("/objectReturn")
+	public String objectReturn(String chNum, Model model) {
+		
+		model.addAttribute("chNum", chNum);
+		return "ch02/objectReturn";
+	}
+	
+	@GetMapping(value="/objectReturnJson1", produces="application/json; charset=UTF-8")
+	@ResponseBody // 리턴된 객체를 JSON으로 해석하고 응답 본문에 넣겠다
+	public String objectReturnJson1() {
+		log.info("objectReturnJson1() 실행");
+		
+		JSONObject jsonObject = new JSONObject();
+		jsonObject.put("fileName", "photo1.jpg");
+		return jsonObject.toString();
+	}
+	
+	@GetMapping(value="/objectReturnJson2", produces="application/json; charset=UTF-8")
+	@ResponseBody // 리턴된 객체를 JSON으로 변환하고 응답 본문에 넣겠다
+	public FileInfo objectReturnJson2() {
+		log.info("objectReturnJson2() 실행");
+		FileInfo fi = new FileInfo();
+		fi.setFileName("photo2.jpg");
+		fi.setInfo("아름다운 풍경 사진");
+		return fi;
+	}
+	
+	@GetMapping("/testAuthInterceptor1")
+	public String testAuthInterceptor1(String chNum, Model model) {
+		log.info("testAuthInterceptor1() 실행");
+		model.addAttribute("chNum", chNum);
+		return "ch02/testAuthInterceptor1";
+	}
+	
+	@GetMapping("/testAuthInterceptor2")
+	@Auth
+	public String testAuthInterceptor2(String chNum, Model model) {
+		log.info("testAuthInterceptor2() 실행");
+		model.addAttribute("chNum", chNum);
+		return "ch02/testAuthInterceptor2";
+	}
 }
